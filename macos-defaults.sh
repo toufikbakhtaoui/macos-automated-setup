c9752350bdaba0c8f8609ce8719a801191112e6d
toufik Bakhtaoui <toufik.bakhtaoui@gmail.com>
2025-03-09 00:28:56 +0100

    add settings to macos defaults

--- macos-defaults.sh
+++ macos-defaults.sh
@@ -1,6 +1,22 @@
-# show hidden files in the finder
+# Show hidden files in the Finder
 defaults write com.apple.finder AppleShowAllFiles -bool true
+
+# --------------------Dock-----------------------------------
+# Set the icon size of Dock items in pixels
+defaults write com.apple.dock "tilesize" -int "30"
+
+# Autohide
+defaults write com.apple.dock "autohide" -bool "true"
+
+# Do not display recent apps in the Dock
+defaults write com.apple.dock "show-recents" -bool "false"
+
+# --------------------Finder----------------------------------
+# Show all file extensions inside the Finder
+defaults write NSGlobalDomain "AppleShowAllExtensions" -bool "true"
 
 # restart the dock and the finder
 killall Dock
 killall Finder
+
+echo "MacOS defaults settings was successfully executed !"
