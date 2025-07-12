682fbe19841afc37bef22554587eebc335f37f47
toufik Bakhtaoui <toufik.bakhtaoui@gmail.com>
2025-07-12 02:59:25 +0200

    delete diagnostics

--- dotfiles/nvim/.config/nvim/lua/core/keymaps.lua
+++ dotfiles/nvim/.config/nvim/lua/core/keymaps.lua
@@ -67,9 +67,3 @@ vim.keymap.set('v', '>', '>gv', opts)
 
 -- Keep last yanked when pasting
 vim.keymap.set('v', 'p', '"_dP', opts)
-
--- Diagnostic keymaps
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous diagnostic message' })
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
-vim.keymap.set('n', '<leader>d', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
-vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
